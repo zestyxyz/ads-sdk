1b360709dcbbdd14de508087b93b67f0f87c6611
Daniel Adams <msub2official@gmail.com>
2021-06-30 16:04:26 -0400

    Update .gitignore/deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,11 @@ cd utils && yarn
 cd ..
 cd aframe && yarn && yarn run build
 cd ..
+cd r3f && yarn && yarn run build
+cd ..
 cd threejs && yarn && yarn run build
 cd ..
 cp -R aframe/dist dist
+cp -R r3f/dist dist
 cp -R threejs/dist/* dist
 cp -R assets dist

cf6d7aad4c8dcc6f72f6ee6e4d66f6851ea435be
elijah <laughinglemur@protonmail.com>
2021-07-06 15:01:48 -0400

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,11 @@ cd r3f && yarn && yarn run build
 cd ..
 cd threejs && yarn && yarn run build
 cd ..
+cd web && yarn && yarn run build
+cd ..
+
 cp -R aframe/dist dist
 cp -R r3f/dist/* dist
 cp -R threejs/dist/* dist
+cp -R web/dist/* dist
 cp -R assets dist
--- web/dist/zesty-web.js
+++
@@ -1,1 +0,0 @@
-(()=>{class e extends HTMLElement{constructor(){super()}render(){this.innerHTML="\n            <div>\n                <h1>Zesty</h1>\n            </div>\n        "}}customElements.define("zesty",e)})();

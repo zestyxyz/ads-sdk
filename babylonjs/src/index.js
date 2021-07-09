389fc4646900dba3650f0df9e937092e783dd121
Daniel Adams <msub2official@gmail.com>
2021-07-09 18:41:29 -0400

    First commit, basic functionality

---
+++ babylonjs/src/index.js
@@ -0,0 +1,42 @@
+import { fetchNFT, fetchActiveAd, sendMetric } from '../../utils/networking';
+//import * as BABYLON from 'babylonjs';
+
+export default class ZestyAd {
+  constructor(width, height, adSpace, creator) {
+    const options = {
+      height: height,
+      width: width
+    }
+    let zestyad = BABYLON.MeshBuilder.CreatePlane('zestyad', options);
+    zestyad.position = new BABYLON.Vector3(0, 2, 2);
+    loadAd(adSpace, creator).then(data => {
+      zestyad.material = data.mat;
+      console.log(data.mat);
+    })
+  }
+
+  onclick() {
+
+  }    
+}
+
+async function loadAd(adSpace, creator) {
+  const activeNFT = await fetchNFT(adSpace, creator);
+  console.log(activeNFT);
+  const activeAd = await fetchActiveAd(activeNFT.uri);
+  console.log(activeAd);
+
+  // Need to add https:// if missing for page to open properly
+  let url = activeAd.data.url;
+  url = url.match(/^http[s]?:\/\//) ? url : 'https://' + url;
+
+  let image = activeAd.data.image;
+  image = image.match(/^.+\.(png|jpe?g)/i) ? image : `https://ipfs.zesty.market/ipfs/${image}`;
+
+  const mat = new BABYLON.StandardMaterial('');
+  mat.diffuseTexture = new BABYLON.Texture(image);
+
+  return { mat: mat, src: image, uri: activeAd.uri, url: url }
+}
+
+window.ZestyAd = ZestyAd;

ca1c31dc37d91384be484a796bc438afac0ad0a0
Daniel Adams <msub2official@gmail.com>
2021-07-13 15:56:58 -0400

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,15 +4,21 @@ cd utils && yarn
 cd ..
 cd aframe && yarn && yarn run build
 cd ..
+cd babylonjs && yarn && yarn run build
+cd ..
 cd r3f && yarn && yarn run build
 cd ..
 cd threejs && yarn && yarn run build
 cd ..
 cd web && yarn && yarn run build
 cd ..
+cd wonderland && yarn && yarn run build
+cd ..
 
 cp -R aframe/dist dist
+cp -R babylonjs/dist/* dist
 cp -R r3f/dist/* dist
 cp -R threejs/dist/* dist
 cp -R web/dist/* dist
+cp -R wonderland/dist/* dist
 cp -R assets dist

8c07322d8d24fadc9c877e402e71069e7907006d
Daniel Adams <msub2official@gmail.com>
2021-09-08 15:57:01 -0400

    Parsing + IP hashing (no node route handling yet)

--- unity/Assets/ZestySDK/Scripts/Internal/Tracking/User/Session.cs
+++ unity/Assets/ZestySDK/Scripts/Internal/Tracking/User/Session.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Net;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Zesty {
     [DisallowMultipleComponent]
@@ -12,6 +15,8 @@ namespace Zesty {
         [HideInInspector]
         public string sessionID;
 
+        private byte[] signedMessage;
+
         void Awake () {
             if (Instance != null && Instance != this) {
                 Destroy (gameObject);
@@ -20,16 +25,31 @@ namespace Zesty {
             }
         }
 
-        void Start () {
-            CreateNewSession ();
+        void Start() {
+            CreateNewSession();
         }
 
-        public void CreateNewSession () {
+        public void CreateNewSession() {
             sessionID = Guid.NewGuid ().ToString ();
 
             if (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.LinuxEditor) {
                 Debug.Log ($@"[NEW_SESSION] SESSION_ID: {sessionID}");
 
+            }
+
+            string[] key = { "ip" };
+            StartCoroutine(API.GetRequest("https://api.myip.com", key, sendIP));
+        }
+
+        private void sendIP(Dictionary<string, string> info)
+        {
+            byte[] ip = Encoding.ASCII.GetBytes(info["ip"]);
+            using (SHA256CryptoServiceProvider sha = new SHA256CryptoServiceProvider())
+            {
+                byte[] hashedIP = sha.ComputeHash(ip);
+                string hashedIPString = Encoding.ASCII.GetString(hashedIP);
+                Debug.Log(hashedIPString);
+                StartCoroutine(API.PostRequest(Constants.DEV_API_METRICS_URL, hashedIPString, null, null));
             }
         }
 

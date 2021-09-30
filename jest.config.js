9b05452208c62efc2edc78f57f7d34822631427e
Daniel Adams <msub2official@gmail.com>
2021-09-30 11:04:01 -0400

    Trying to get wonderland tests working

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 const config = {
     verbose: true,
-    preset: "jest-puppeteer"
+    preset: "jest-puppeteer",
+    globals: {
+        "WL": "window.WL",
+        "WL.Type.Object": "window.WL.Type.Object"
+    }
 }
 
 module.exports = config;
--- tests/wonderland.test.js
+++ tests/wonderland.test.js
@@ -1,5 +1,16 @@
 import { expect, test, describe, jest, beforeAll } from '@jest/globals';
 
-test('test', () => {
-    expect(true);
+jest.setTimeout(10000);
+
+describe('Initial load', () => {
+    test('The correct test page is currently loaded', async () => {
+        await page.goto('http://localhost:8080/tests/wonderland/deploy');
+        await expect(page.title()).resolves.toBe('Wonderland Test');
+    });
+    /*
+    test('All 9 banners are currently loaded', async () => {
+        await page.waitForTimeout(6000);
+        const bannerCount = await page.evaluate(() => window.testBanners.length);
+        expect(bannerCount).toBe(9);
+    });*/
 })

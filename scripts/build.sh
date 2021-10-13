195de1954f2cdd7592fb5b7ca82b9520d388b730
Daniel Adams <msub2official@gmail.com>
2021-10-13 18:48:33 -0400

    Just move everything back into one script

--- scripts/build.sh
+++ scripts/build.sh
@@ -19,3 +19,12 @@ cd ..
 
 # Test
 yarn run test
+
+# Deploy
+cp -R aframe/dist dist
+cp -R babylonjs/dist/* dist
+cp -R r3f/dist/* dist
+cp -R threejs/dist/* dist
+cp -R web/dist/* dist
+cp -R wonderland/dist/* dist
+cp -R assets dist
--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp -R aframe/dist dist
-cp -R babylonjs/dist/* dist
-cp -R r3f/dist/* dist
-cp -R threejs/dist/* dist
-cp -R web/dist/* dist
-cp -R wonderland/dist/* dist
-cp -R assets dist

012398ae88087d98f973e1e233b9a0c7158d834f
Daniel Adams <msub2official@gmail.com>
2021-10-13 18:20:06 -0400

    Update workflow scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# Install dependencies
-yarn
-cd utils && yarn
-cd ..
-cd aframe && yarn && yarn run build
-cd ..
-cd babylonjs && yarn && yarn run build
-cd ..
-cd r3f && yarn && yarn run build
-cd ..
-cd threejs && yarn && yarn run build
-cd ..
-cd web && yarn && yarn run build
-cd ..
-cd wonderland && yarn && yarn run build
-cd ..
-
-# Test
-yarn run test
-
 cp -R aframe/dist dist
 cp -R babylonjs/dist/* dist
 cp -R r3f/dist/* dist
---
+++ scripts/test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Test
+yarn run test

9bc5d3970d5fb14c7174fc799fe65098e287549c
Daniel Adams <msub2official@gmail.com>
2021-10-14 15:59:38 -0400

    Try to use caching and separate jobs again

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,15 +16,3 @@ cd web && yarn && yarn run build
 cd ..
 cd wonderland && yarn && yarn run build
 cd ..
-
-# Test
-yarn run test
-
-# Deploy
-cp -R aframe/dist dist
-cp -R babylonjs/dist/* dist
-cp -R r3f/dist/* dist
-cp -R threejs/dist/* dist
-cp -R web/dist/* dist
-cp -R wonderland/dist/* dist
-cp -R assets dist
---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Deploy
+cp -R aframe/dist dist
+cp -R babylonjs/dist/* dist
+cp -R r3f/dist/* dist
+cp -R threejs/dist/* dist
+cp -R web/dist/* dist
+cp -R wonderland/dist/* dist
+cp -R assets dist

9b9af1477e8384d79b35c922b712d88b74e770ef
Daniel Adams <msub2official@gmail.com>
2021-11-01 17:35:10 -0400

    Fix build scripts

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd utils && yarn
+cd ..
+cd aframe && yarn && yarn run build
+cd ..
+cd babylonjs && yarn && yarn run build
+cd ..
+cd r3f && yarn && yarn run build
+cd ..
+cd threejs && yarn && yarn run build
+cd ..
+cd web && yarn && yarn run build
+cd ..
+cd wonderland && yarn && yarn run build
+cd ..
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-cd utils && yarn
-cd ..
-cd aframe && yarn && yarn run build
-cd ..
-cd babylonjs && yarn && yarn run build
-cd ..
-cd r3f && yarn && yarn run build
-cd ..
-cd threejs && yarn && yarn run build
-cd ..
-cd web && yarn && yarn run build
-cd ..
-cd wonderland && yarn && yarn run build
-cd ..
-
 cp -R aframe/dist dist
 cp -R babylonjs/dist/* dist
 cp -R r3f/dist/* dist

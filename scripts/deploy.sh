2e36756956d862e2e4e43c7d3c6ffa092a33dd6b
Daniel Adams <msub2official@gmail.com>
2021-11-09 15:53:26 -0500

    Add unity package to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -19,6 +19,7 @@ cp -R aframe/dist dist
 cp -R babylonjs/dist/* dist
 cp -R r3f/dist/* dist
 cp -R threejs/dist/* dist
+cp unity/zesty-unity-sdk.unitypackage dist
 cp -R web/dist/* dist
 cp -R wonderland/dist/* dist
 cp -R assets dist

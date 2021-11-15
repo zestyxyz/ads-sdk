923204b863795104a8f9f7031fb009ea2926bd05
zestified <93493166+zestified@users.noreply.github.com>
2021-11-14 22:45:12 -0500

    Introduce timestamp

--- utils/database.js
+++ utils/database.js
@@ -25,16 +25,24 @@ class Database {
         return this.metrics.get(adSpace)
     }
 
-    async setAdSpace(adSpace, value) {
-        return await this.metrics.set(adSpace, { timestamp: new Date().toString(), count: value })
+    async setAdSpace(adSpace, data) {
+        return await this.metrics.set(adSpace, data)
     }
 
     async increment(adSpace) {
         const as = this.getAdSpace(adSpace)
+        const dateString = new Date(2021, 11, 16).toDateString()
         if(!as) {
-            return await this.setAdSpace(adSpace, 0) // CID
+            let data = {}
+            data[dateString] = 1
+            return await this.setAdSpace(adSpace, data) // CID
         } else {
-            return await this.setAdSpace(adSpace, as.count + 1)
+            if(as[dateString]) {
+                as[dateString] += 1
+            } else {
+                as[dateString] = 1
+            }
+            return await this.setAdSpace(adSpace, as)
         }
     }
 

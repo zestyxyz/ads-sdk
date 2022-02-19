88e95d75702c4df997c338e518fee4778bb78189
Srile <florianisk@yahoo.com>
2022-02-19 11:13:35 +0700

    Add r3f to build script, add build_publish

--- scripts/build_publish.js
+++ scripts/build_publish.js
@@ -7,3 +7,10 @@ console.log('Building r3f:\n' + execSync('cd r3f && yarn build').toString());
 console.log('Building Web:\n' + execSync('cd web && yarn build').toString());
 console.log('Building Wonderland:\n' + execSync('cd wonderland && yarn build').toString());
 console.log('Building zestyFormats:\n' + execSync('cd utils && yarn build').toString());
+
+console.log('Publishing A-Frame:\n' + execSync('cd aframe && npm publish --access public').toString());
+console.log('Publishing babylon.js:\n' + execSync('cd babylonjs && npm publish --access public').toString());
+console.log('Publishing three.js:\n' + execSync('cd threejs && npm publish --access public').toString());
+console.log('Publishing r3f:\n' + execSync('cd r3f && npm publish --access public').toString());
+console.log('Publishing Web:\n' + execSync('cd web && npm publish --access public').toString());
+console.log('Publishing Wonderland:\n' + execSync('cd wonderland && npm publish --access public').toString());

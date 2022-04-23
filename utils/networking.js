2abc654478d107cff8d808ceceb958db1b2973ae
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2022-04-24 03:56:48 +0800

    updated networking script

--- utils/networking.js
+++ utils/networking.js
@@ -59,6 +59,7 @@ const fetchNFT = async (space, creator, network = 'polygon') => {
                 uri
               }
               buyerCampaignsApproved
+              buyerCampaignsIdList
             }
           }
           id

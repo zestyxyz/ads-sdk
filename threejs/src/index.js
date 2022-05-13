66941efe40c352276024d6b7b63181052c534e08
Daniel Adams <msub2official@gmail.com>
2022-05-13 14:26:43 -0400

    Remove creator field from threejs

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -10,25 +10,23 @@ export default class ZestyBanner extends THREE.Mesh {
   /**
    * @constructor
    * @param {string} space The space ID
-   * @param {string} creator The wallet ID of the creator
    * @param {string} network The network to connect to ('rinkeby' or 'polygon')
    * @param {string} format The format of the default banner
    * @param {Number} height Height of the banner
    * @param {THREE.WebGLRenderer} renderer Optional field to pass in the WebGLRenderer in a WebXR project
    */
-  constructor(space, creator, network, format, style, height, renderer = null, beacon = true) {
+  constructor(space, network, format, style, height, renderer = null, beacon = true) {
     super();
     this.geometry = new THREE.PlaneGeometry(formats[format].width * height, height, 1, 1);
 
     this.type = 'ZestyBanner';
     this.space = space;
-    this.creator = creator;
     this.network = network;
     this.renderer = renderer;
     this.beacon = beacon;
     this.banner = {};
 
-    this.bannerPromise = loadBanner(space, creator, network, format, style).then(banner => {
+    this.bannerPromise = loadBanner(space, network, format, style).then(banner => {
       this.material = new THREE.MeshBasicMaterial({
         map: banner.texture
       });
@@ -56,8 +54,8 @@ export default class ZestyBanner extends THREE.Mesh {
   }
 }
 
-async function loadBanner(space, creator, network, format, style) {
-  const activeNFT = await fetchNFT(space, creator, network);
+async function loadBanner(space, network, format, style) {
+  const activeNFT = await fetchNFT(space, network);
   const activeBanner = await fetchActiveBanner(activeNFT.uri, format, style, space);
 
   // Need to add https:// if missing for page to open properly

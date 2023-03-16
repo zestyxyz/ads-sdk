ae9388a48697e4e5b6bd43a464eda30b5408fc96
Daniel Adams <msub2official@gmail.com>
2023-03-16 14:18:23 -0400

    Update babylon.js to 2.0.0

--- babylonjs/src/index.js
+++ babylonjs/src/index.js
@@ -1,6 +1,6 @@
 /* global BABYLON */
 
-import { fetchNFT, fetchActiveBanner, sendOnLoadMetric, sendOnClickMetric } from '../../utils/networking';
+import { fetchCampaignAd, fetchNFT, fetchActiveBanner, sendOnLoadMetric, sendOnClickMetric } from '../../utils/networking';
 import { formats } from '../../utils/formats';
 import { openURL, parseProtocol } from '../../utils/helpers';
 import { version } from '../package.json';
@@ -8,7 +8,7 @@ import { version } from '../package.json';
 console.log('Zesty SDK Version: ', version);
 
 export default class ZestyBanner {
-  constructor(space, network, format, style, height, scene, webXRExperienceHelper = null, beacon = true) {
+  constructor(space, format, style, height, scene, webXRExperienceHelper = null, beacon = true) {
     const options = {
       height: height,
       width: formats[format].width * height
@@ -16,7 +16,7 @@ export default class ZestyBanner {
 
     this.zestyBanner = BABYLON.MeshBuilder.CreatePlane('zestybanner', options);
 
-    loadBanner(space, network, format, style).then(data => {
+    loadBanner(space, format, style).then(data => {
       this.zestyBanner.material = data.mat;
       this.zestyBanner.actionManager = new BABYLON.ActionManager(scene);
 
@@ -44,11 +44,10 @@ export default class ZestyBanner {
   }
 }
 
-async function loadBanner(space, network, format, style) {
-  const activeNFT = await fetchNFT(space, network);
-  const activeBanner = await fetchActiveBanner(activeNFT.uri, format, style, space);
+async function loadBanner(space, format, style) {
+  const activeBanner = await fetchCampaignAd(space, format, style);
 
-  const { image, url } = activeBanner.data;
+  const { asset_url: image, cta_url: url } = activeBanner[0];
 
   const mat = new BABYLON.StandardMaterial('');
   mat.diffuseTexture = new BABYLON.Texture(image);

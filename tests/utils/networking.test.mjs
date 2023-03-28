f9c4f5cdd9e283fbc830ea6a7893d02821044d3a
Daniel Adams <msub2official@gmail.com>
2023-03-28 11:15:36 -0400

    Update default banner to prod S3 link

--- tests/utils/networking.test.mjs
+++ tests/utils/networking.test.mjs
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { fetchCampaignAd } from '../../utils/networking.js';
-const DEFAULT_BANNER = 'https://zesty-storage-09e38383232656-staging.s3.amazonaws.com/images/zesty/zesty-banner-tall.png'
+const DEFAULT_BANNER = 'https://zesty-storage-prod.s3.amazonaws.com/images/zesty/zesty-banner-tall.png'
 
 test.describe('fetchCampaignAd', () => {
   test('fetchCampaignAd should return a default banner if no URI is given', () => {

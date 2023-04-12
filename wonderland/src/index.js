4bd5d66c7c15853a4d1a6e5029b359d1662ba5e6
Daniel Adams <msub2official@gmail.com>
2023-04-12 13:03:36 -0400

    Update WLE SDK for 1.0

--- wonderland/src/index.js
+++ wonderland/src/index.js
@@ -4,6 +4,14 @@ import { fetchCampaignAd, sendOnLoadMetric, sendOnClickMetric } from '../../util
 import { formats, defaultFormat } from '../../utils/formats';
 import { openURL } from '../../utils/helpers';
 import { version } from '../package.json';
+import {
+  Component,
+  Collider,
+  MeshComponent,
+  CollisionComponent,
+  Property
+} from '@wonderlandengine/api';
+import { CursorTarget } from '@wonderlandengine/components';
 
 console.log('Zesty SDK Version: ', version);
 
@@ -15,142 +23,139 @@ console.log('Zesty SDK Version: ', version);
  *
  * Make sure that you set up a cursor to enable clicking.
  */
-WL.registerComponent(
-  'zesty-banner',
-  {
+export class ZestyBanner extends Component {
+  static TypeName = 'zesty-banner';
+  static Properties = {
     /* Your banner space index */
-    space: { type: WL.Type.String },
+    space: Property.int(),
     /* The default banner format, determines aspect ratio */
-    format: { type: WL.Type.Enum, values: Object.keys(formats), default: defaultFormat },
+    format: Property.enum(['tall', 'wide', 'square'], 'square'),
     /* The default banner visual style */
-    style: {
-      type: WL.Type.Enum,
-      values: ['standard', 'minimal', 'transparent'],
-      default: 'transparent',
-    },
+    style: Property.enum(['standard', 'minimal', 'transparent'], 'transparent'),
     /* Scale width of the object to banner ratio (see format) and set the collider */
-    scaleToRatio: { type: WL.Type.Bool, default: true },
+    scaleToRatio: Property.bool(true),
     /* Texture property to set after banner is loaded. Leave "auto" to detect from
-     * known pipelines (Phong Opaque Textured, Flat Opaque Textured) */
-    textureProperty: { type: WL.Type.String, default: 'auto' },
-    beacon: { type: WL.Type.Bool, default: true },
+    * known pipelines (Phong Opaque Textured, Flat Opaque Textured) */
+    textureProperty: Property.string('auto'),
+    /* Whether to assign the banner to the alphaMaskTexture property of the material */
+    assignAlphaMaskTexture: Property.bool(true),
+    beacon: Property.bool(true),
     /* Load IPFS gateways and default image uris at runtime, if false at build time */
-    dynamicFormats: { type: WL.Type.Bool, default: true },
+    dynamicFormats: Property.bool(true),
     /* Automatically creates a collision and cursor-target components, if there isn't one */
-    createAutomaticCollision: { type: WL.Type.Bool, default: true },
-  },
-  {
-    init: function () {
-      this.formats = Object.values(formats);
-      this.formatKeys = Object.keys(formats);
-      this.styleKeys = ['standard', 'minimal', 'transparent'];
-    },
+    createAutomaticCollision: Property.bool(true)
+  };
 
-    start: function () {
-      this.mesh = this.object.getComponent('mesh');
-      if (!this.mesh) {
-        throw new Error("'zesty-banner ' missing mesh component");
-      }
-
-      if(this.createAutomaticCollision) {
-        this.collision =
-          this.object.getComponent('collision') ||
-          this.object.addComponent('collision', {
-            collider: WL.Collider.Box,
-            group: 0x2,
-          });
-
-        this.cursorTarget =
-          this.object.getComponent('cursor-target') || this.object.addComponent('cursor-target');
-        this.cursorTarget.addClickFunction(this.onClick.bind(this));
-      }
-
-
-      if(this.dynamicFormats) {
-        let formatsScript = document.createElement('script');
-
-        formatsScript.onload = () => {
-          this.formatsOverride = zestyFormats.formats;
-          this.startLoading();
-        }
-        formatsScript.setAttribute('src', 'https://ipfs.io/ipns/lib.zesty.market/zesty-formats.js');
-        formatsScript.setAttribute('crossorigin', 'anonymous');
-        document.body.appendChild(formatsScript);
-      } else {
-        this.startLoading();
-      }
-    },
-    startLoading: function() {
-      this.loadBanner(
-        this.space,
-        this.formatKeys[this.format],
-        this.styleKeys[this.style]
-      ).then((banner) => {
-        this.banner = banner;
-
-        if (this.scaleToRatio) {
-          /* Make banner always 1 meter height, adjust width according to banner aspect ratio */
-          this.height = this.object.scalingLocal[1];
-          this.object.resetScaling();
-
-          if(this.createAutomaticCollision) {
-            this.collision.extents = [
-              this.formats[this.format].width * this.height,
-              this.height,
-              0.1,
-            ];
-          }
-          this.object.scale([this.formats[this.format].width * this.height, this.height, 1.0]);
-        }
-        /* WL.Material.shader will be renamed to pipeline at some point,
-         * supporting as many API versions as possible. */
-        const m = this.mesh.material.clone();
-        if (this.textureProperty === 'auto') {
-          const pipeline = m.pipeline || m.shader;
-          if (pipeline === 'Phong Opaque Textured') {
-            m.diffuseTexture = banner.texture;
-            m.alphaMaskThreshold = 0.3;
-          } else if (pipeline === 'Flat Opaque Textured') {
-            m.flatTexture = banner.texture;
-            m.alphaMaskThreshold = 0.8;
-          } else {
-            throw Error(
-              "'zesty-banner ' unable to apply banner texture: unsupported pipeline " + m.shader
-            );
-          }
-          this.mesh.material = m;
-        } else {
-          this.mesh.material[this.textureProperty] = banner.texture;
-        }
-
-        if (this.beacon) {
-          sendOnLoadMetric(this.space, this.banner.campaignId);
-        }
-      });
-    },
-    onClick: function () {
-      if (this.banner?.url) {
-        if (WL.xrSession) {
-          WL.xrSession.end().then(this.executeClick.bind(this));
-        } else {
-          this.executeClick();
-        }
-      }
-    },
-    executeClick: function () {
-      openURL(this.banner.url);
-      if (this.beacon) {
-        sendOnClickMetric(this.space, this.banner.campaignId);
-      }
-    },
-    loadBanner: async function (space, format, style) {
-      const activeCampaign = await fetchCampaignAd(space, format, style)
-
-      const { asset_url: image, cta_url: url } = activeCampaign.Ads[0];
-
-      return WL.textures.load(image, '').then((texture) => {
-        return { texture, imageSrc: image, url, campaignId: activeCampaign.CampaignId };
-      });
-    },
+  init() {
+    this.formats = Object.values(formats);
+    this.formatKeys = Object.keys(formats);
+    this.styleKeys = ['standard', 'minimal', 'transparent'];
   }
-);
+
+  start() {
+    this.mesh = this.object.getComponent(MeshComponent);
+    if (!this.mesh) {
+      throw new Error("'zesty-banner ' missing mesh component");
+    }
+
+    if (this.createAutomaticCollision) {
+      this.collision =
+        this.object.getComponent(CollisionComponent) ||
+        this.object.addComponent(CollisionComponent, {
+          collider: Collider.Box,
+          group: 0x2
+        });
+
+      this.cursorTarget =
+        this.object.getComponent(CursorTarget) || this.object.addComponent(CursorTarget);
+      this.cursorTarget.addClickFunction(this.onClick.bind(this));
+    }
+
+    if (this.dynamicFormats) {
+      let formatsScript = document.createElement('script');
+
+      formatsScript.onload = () => {
+        this.formatsOverride = zestyFormats.formats;
+        this.startLoading();
+      };
+      formatsScript.setAttribute('src', 'https://ipfs.io/ipns/lib.zesty.market/zesty-formats.js');
+      formatsScript.setAttribute('crossorigin', 'anonymous');
+      document.body.appendChild(formatsScript);
+    } else {
+      this.startLoading();
+    }
+  }
+
+  startLoading() {
+    this.loadBanner(
+      this.space,
+      this.formatKeys[this.format],
+      this.styleKeys[this.style]
+    ).then(banner => {
+      this.banner = banner;
+      if (this.scaleToRatio) {
+        /* Make banner always 1 meter height, adjust width according to banner aspect ratio */
+        this.height = this.object.scalingLocal[1];
+        this.object.resetScaling();
+        if (this.createAutomaticCollision) {
+          this.collision.extents = [
+            this.formats[this.format].width * this.height,
+            this.height,
+            0.1
+          ];
+        }
+        this.object.scale([this.formats[this.format].width * this.height, this.height, 1.0]);
+      }
+      const m = this.mesh.material.clone();
+      if (this.textureProperty === 'auto') {
+        const pipeline = m.shader;
+        if (pipeline === 'Phong Opaque Textured') {
+          m.diffuseTexture = banner.texture;
+          m.alphaMaskThreshold = 0.3;
+        } else if (pipeline === 'Flat Opaque Textured') {
+          m.flatTexture = banner.texture;
+          m.alphaMaskThreshold = 0.8;
+        } else {
+          throw Error(
+            "'zesty-banner' unable to apply banner texture: unsupported pipeline " + pipeline
+          );
+        }
+        this.mesh.material = m;
+        this.mesh.material.alphaMaskTexture = banner.texture;
+      } else {
+        this.mesh.material[this.textureProperty] = banner.texture;
+        this.mesh.material.alphaMaskTexture = banner.texture;
+      }
+      if (this.beacon) {
+        sendOnLoadMetric(this.space);
+      }
+    });
+  }
+
+  onClick() {
+    if (this.banner?.url) {
+      if (this.engine.xrSession) {
+        this.engine.xrSession.end().then(this.executeClick.bind(this));
+      } else {
+        this.executeClick();
+      }
+    }
+  }
+
+  executeClick() {
+    openURL(this.banner.url);
+    if (this.beacon) {
+      sendOnClickMetric(this.space);
+    }
+  }
+
+  async loadBanner(space, format, style) {
+    const activeCampaign = await fetchCampaignAd(space, format, style)
+
+    const { asset_url: image, cta_url: url } = activeCampaign.Ads[0];
+
+    return this.engine.textures.load(image, '').then(texture => {
+      return { texture, imageSrc: image, url, campaignId: activeCampaign.CampaignId };
+    });
+  }
+}

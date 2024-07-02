008bb07c4a743ed3a0ba3d499457da6f74a446cb
Daniel Adams <msub2official@gmail.com>
2024-07-01 18:23:44 -1000

    Update git workflows and build script for test projects
    Install yarn
    
    Adjust WLE project path

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-cd utils && yarn
-cd ..
-cd aframe && yarn && yarn run build
-cd ..
-cd babylonjs && yarn && yarn run build
-cd ..
-cd beacon && yarn && yarn run build
-cd ..
-cd r3f && yarn && yarn run build
-cd ..
-cd threejs && yarn && yarn run build
-cd ..
-cd web && yarn && yarn run build
-cd ..
-cd wonderland && yarn && yarn run build
-cd ..
-cd utils && yarn && yarn run build && yarn run build-networking
-cd ..
+cd utils && yarn && cd ..
+cd aframe && yarn && yarn run build && cd ..
+cd babylonjs && yarn && yarn run build && cd ..
+cd beacon && yarn && yarn run build && cd ..
+cd r3f && yarn && yarn run build && cp dist/* ../tests/r3f/src && cd ..
+cd threejs && yarn && yarn run build && cd ..
+cd web && yarn && yarn run build && cd ..
+cd wonderland && yarn && yarn run build && cd ..
+cd utils && yarn && yarn run build && yarn run build-networking && cd ..
+cd tests/r3f && yarn && yarn run build && cd ..

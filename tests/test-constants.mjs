1435f48a38962ba4d783a4542373ee6aa6b641f9
Daniel Adams <msub2official@gmail.com>
2024-07-12 08:49:44 -1000

    Lower refresh test wait interval

--- tests/test-constants.mjs
+++ tests/test-constants.mjs
@@ -2,7 +2,7 @@ export const EXAMPLE_URL = 'https://example.com';
 export const EXAMPLE_IMAGE = 'https://picsum.photos/300/250';
 export const EXAMPLE_IMAGE2 = 'https://picsum.photos/300/300';
 export const PREBID_LOAD_TEST_WAIT_INTERVAL = 5000;
-export const PREBID_REFRESH_TEST_WAIT_INTERVAL = 10000;
+export const PREBID_REFRESH_TEST_WAIT_INTERVAL = 9000;
 
 export async function injectIFrame(page, url, image) {
   await page.waitForFunction(() => document.querySelector('#zesty-div-medium-rectangle') != null);
